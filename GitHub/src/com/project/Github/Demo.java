49738504cb95dfa2cb41c9abaec7fc8f50826a49
Nikhi <Nikhi@MSI>
2023-12-19 14:16:08 +0530

    commit by nik

---
+++ GitHub/src/com/project/Github/Demo.java
@@ -0,0 +1,16 @@
+package com.project.Github;
+
+public class Demo
+{
+
+	public static void main(String[] args) 
+	{
+		
+		
+		
+		System.out.println("Hello welcome to hEllo world");
+		System.out.println("----Created by Nikhil ");
+		
+	}
+
+}
